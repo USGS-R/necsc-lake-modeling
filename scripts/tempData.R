59241e57c9e4d3a01fc866d7a9746b43db024230
Megan Hines <mhines@usgs.gov>
2015-12-08 13:35:15 -0600

    start of data munging for eventual leaflet map. will eventually break into better functions, right now kind of a pile o' code that needs to be intelligently broken up.

---
+++ scripts/tempData.R
@@ -0,0 +1,66 @@
+##just a pile of experiments at this point
+
+library(dataRetrieval)
+library(dplyr)
+
+#get the water data
+#78 obs 11/20/2015
+temperatureSample <- readWQPdata(statecode="US:27",characteristicName="Temperature, sample", siteType="Lake, Reservoir, Impoundment")
+write.csv(temperatureSample,"temperatureSample.csv",row.names=FALSE)
+
+#733999 obs 11/20/2015
+temperatureWater <- readWQPdata(statecode="US:27",characteristicName="Temperature, water", siteType="Lake, Reservoir, Impoundment")
+write.csv(temperatureWater,"temperatureWater.csv",row.names=FALSE)
+
+#subset only the fields we want
+temperatureSample <- read.csv(file = "temperatureSample.csv", sep = ",")[ ,c('ActivityStartDate','ActivityStartTime.Time','ActivityStartTime.TimeZoneCode','ActivityBottomDepthHeightMeasure.MeasureValue','ActivityBottomDepthHeightMeasure.MeasureUnitCode','MonitoringLocationIdentifier','ResultMeasureValue','ResultMeasure.MeasureUnitCode')]
+
+temperatureWater <- read.csv(file = "temperatureWater.csv", sep = ",")[ ,c('ActivityStartDate','ActivityStartTime.Time','ActivityStartTime.TimeZoneCode','ActivityBottomDepthHeightMeasure.MeasureValue','ActivityBottomDepthHeightMeasure.MeasureUnitCode','MonitoringLocationIdentifier','ResultMeasureValue','ResultMeasure.MeasureUnitCode')]
+
+#merge the two files into a dataframe
+tempdf <- as.data.frame(temperatureSample)
+tempdf <- rbind(temperatureWater, tempdf)
+
+#get minnesota site data
+sites <- whatWQPsites(stateCd="MN")
+write.csv(sites,"mnSites.csv",row.names=FALSE)
+#subset minnesota site data
+sitesMn <- read.csv(file="mnSites.csv",sep=",")[ ,c('MonitoringLocationIdentifier','MonitoringLocationName','ProviderName','LatitudeMeasure','LongitudeMeasure')]
+
+#join site data to temperature data
+#merge(df1, df2, by = "CustomerId")
+merged <- merge(tempdf, as.data.frame(sitesMn), by="MonitoringLocationIdentifier")
+
+#get site count -- how many records total do we have?
+tempMn <- group_by(merged, MonitoringLocationIdentifier)
+siteCount <- summarise(tempMn,
+                   count=n())
+#add to original table
+tempdf <- merge(merged, as.data.frame(siteCount), by="MonitoringLocationIdentifier")
+
+#get unique temp dates per site 
+dateUn <- group_by(merged, MonitoringLocationIdentifier, ActivityStartDate)
+dateCount <- summarise(dateUn,
+                       count=n())
+
+#get number of unique dates for samples per site - how many unique sampling dates do we have?
+bydate <- group_by(dateCount, MonitoringLocationIdentifier)
+summary.bydate <- summarise(bydate,
+                            count.ActivityStartDate=n())
+
+#add to original table
+tempdf <- merge(tempdf, as.data.frame(summary.bydate), by="MonitoringLocationIdentifier")
+
+#save it
+write.csv(tempdf,"sitetempdata.csv",row.names=FALSE)
+
+
+# doesn't work but this is what I want to do
+getDates <- function(tempdf, site) {
+  
+  earliest <- filter(tempdf, MonitoringLocationIdentifier==site)
+  latest <- filter(tempdf, MonitoringLocationIdentifier==site & max(ActivityStartDate.x))
+
+  return(list(earliest=earliest,latest=latest))
+  
+}
