6e41aae8f4357f964f6f3e8ec28d876fd0959843
Megan Hines <mhines@usgs.gov>
2015-12-28 15:18:04 -0600

    improvements

--- scripts/getNhdData.R
+++ scripts/getNhdData.R
@@ -1,4 +1,13 @@
+# gets the data based on states included in the yaml file
+# unzips the geodatabases
+# extracts the NHDWaterbody layer
+# adds a column for the state name
+# writes out a shapefile
+
 library(yaml)
+library(rgdal)
+library(plyr)
+library(dplyr)
 
 config = yaml.load_file("config.yml")
 serviceEndpoint <- config$serviceEndpoint
@@ -13,5 +22,8 @@ for (i in 1:length(states)) {
   filename[i] <- paste0(prefix,"_",res,"_",stateList[2],"_",stateList[1],"_",suffix)
   download.file(url=paste0(serviceEndpoint,filename[i]), destfile = paste0(getwd(),"/data/",filename[i]), method="libcurl", quiet=FALSE)
   unzip(zipfile = paste0(getwd(),"/data/",filename[i]), exdir="data")
+  split <- unlist(strsplit(filename[i],"\\."))
+  fc <- readOGR(dsn=paste0(getwd(),"/data/",split[[1]],"/",split[[1]],".gdb"),layer="NHDWaterbody")
+  fc$state <- as.character(states[[i]][1])
+  writeOGR(fc,dsn = paste0(getwd(),"/data"), driver = "ESRI Shapefile",layer=paste0("NHDWaterbody_",stateList[1]),overwrite_layer = TRUE)
 }
-
