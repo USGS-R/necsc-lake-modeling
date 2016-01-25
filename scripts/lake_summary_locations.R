ffe21ac26ec94ff287f8b4663045d0cc9b1abb28
Jordan S Read <jread@usgs.gov>
2016-01-25 17:01:20 -0600

    make for .RData drivers

--- scripts/lake_summary_locations.R
+++ scripts/lake_summary_locations.R
@@ -4,6 +4,15 @@
 #' 
 lake_summary_locations <- function(){
   
-  return(data.frame(permID=c('perm.1','perm.23', lat=c(42.3,45.6), lon=c(-89.1,-93.1))))
+  # --- PLACEHOLDER!!!
+  warning('placeholder code')
+  return(data.frame(permID=c('perm.1','perm.23'), lat=c(42.3,45.6), lon=c(-89.1,-93.1)))
   #return(simplegeom(data.frame(point1=c(-89.1,43.2), point2=c(-93.1, 45.1))))
+}
+
+stencil_from_id <- function(permIDs){
+  
+  # --- PLACEHOLDER!!!
+  warning('placeholder code')
+  simplegeom(data.frame('perm.1'=c(-89.1,43.2), 'perm.23'=c(-93.1, 45.1)))
 }
--- scripts/lake_summary_nldas.R
+++ scripts/lake_summary_nldas.R
@@ -5,15 +5,31 @@ load_config <- function(data.source="configs/NLDAS_config.yml"){
   yaml.load_file(data.source)
 }
 
-lake_summary_nldas <- function(project_lake_locations, config){
-  stop()
+lake_driver_nldas <- function(file='data/NLDAS_data/NLDAS_driver_file_list.tsv'){
+  mssg.file <- 'data/NLDAS_data/NLDAS_driver_status.txt'
+  files <- strsplit(readLines(file, n = -1),'\t')[[1]]
+  #server.files <- nldas_server_files()
+  cat('index of files contains', length(files), file=mssg.file, append = FALSE)
+  
+  perm.ids <- unique(sapply(strsplit(files,'[_]'),function(x)x[2]))
+  
+  # APPEND files? 
+  
+  #new.files <- setdiff(files, server.files)
+  #rm.files <- setdiff(server.files, files)
+  new.files <- files
+  
+  config <- load_config("configs/NLDAS_config.yml")
+  
+  cat(sprintf('\n%s files are new...',length(new.files)), file=mssg.file, append = TRUE)
+  
   knife = webprocess(url=config$wps_url)
   
   fabric = webdata(url=config$data_url, variable=config$data_variables, times=config$data_times)
   
   # here we should check what files already exist and pare down the requests to be shaped
   
-  job <- geoknife(stencil=project_lake_locations, fabric, knife, wait=TRUE)
+  job <- geoknife(stencil=stencil_from_id(perm.ids), fabric, knife, wait=TRUE)
   if (successful(job)){
     data = result(job, with.units=TRUE)
     features = head(names(data)[-1],-3)
