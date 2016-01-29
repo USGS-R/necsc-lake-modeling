ddaa57ab7df2da274668cd3c2c86828df4497b2c
Megan Hines <mhines@usgs.gov>
2016-01-29 08:51:20 -0600

    too harsh for na removal, needed to limit the columns before using it otherwise lost a lot of data.

--- scripts/depthNHD.R
+++ scripts/depthNHD.R
@@ -1,10 +1,12 @@
 library(rgdal)
 
-#get nhd later
+#get nhd layer
 nhd <- readOGR(dsn = paste0(getwd(),"/data"), layer="NHDWaterbody")
 
+#read in lagos data
 input <- read.delim("lake_info_subset.tsv")
 
+#get the matches between the lat lng pairs and nhd layer
 for (i in 1:nrow(input)) {
   x <- as.numeric(input$nhd_long[i])
   y <- as.numeric(input$nhd_lat[i])
@@ -16,7 +18,12 @@ for (i in 1:nrow(input)) {
   input$permID[i] <- prmnn_i
 }
 input<-as.data.frame(input)
+#write out file for LW with permid and maxdepth
 write.csv(input[,c("permID","maxdepth")], file="lagosNHD.csv",row.names=FALSE) 
 
-keepers <- subset(input, !is.na(maxdepth))
-write.csv(keepers[,c("permID")], file="lagosPermID.csv",row.names=FALSE)
+#write out file for JR with just permids where we have depth data
+keepers <- as.data.frame(input)
+cols <- c("permID")
+keepers <- keepers[,cols,drop=FALSE]
+keepers <- subset(keepers, !is.na(permID))
+write.csv(keepers, file="lagosPermID.csv",row.names=FALSE)
