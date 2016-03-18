6891c30dede5600097ee6b3f0634a7e769abda56
Luke Winslow <lwinslow@usgs.gov>
2016-03-18 14:37:48 -0500

    add kevin MI and SWIMMS depth data

--- scripts/depth_summarize.R
+++ scripts/depth_summarize.R
@@ -1,10 +1,12 @@
 depth_summarize = function(config){
 	
-	data_j = read.table('data/depth_data_linked/depth_herb_jacobson.tsv', sep='\t', header=TRUE)
+	data_j = read.table('data/depth_data_linked/depth_herb_jacobson.tsv', sep='\t', header=TRUE, as.is=TRUE)
 	
 	data_l = read_aes('data/depth_data_linked/depth_lagos.edt', config$LAGOS_KEY)
 	
-	to_write = rbind(data_j, data_l)
+	data_s = read.table('data/depth_data_linked/depth_swimms_wisconsin.csv', sep=',', header=TRUE, as.is=TRUE)
+	
+	to_write = rbind(data_j, data_l, data_s)
 	to_write = to_write[, c('id', 'source', 'type', 'zmax')]
 	
 	write_aes(to_write, 'data/depth_data_linked/all_depths.edt', key = config$LAGOS_KEY)
