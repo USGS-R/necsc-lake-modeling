75f564b64564c920e29aa871785427afff432179
Jordan S Read <jread@usgs.gov>
2016-07-05 08:31:32 -0500

    update w/ generic text that is shared

--- scripts/render_FGDC_metadata.R
+++ scripts/render_FGDC_metadata.R
@@ -3,8 +3,9 @@
 #' @param text data (list) used to fill in template keys
 #' @param file.out file to write the output to
 #' @param scripted.text a second list to be appended to \code{text} that is programatically generated
-render_FGDC_metadata <- function(template.file, text, file.out, scripted.text = NULL){
+render_FGDC_metadata <- function(template.file, text, file.out, scripted.text = NULL, ...){
   render.text <- append(text, scripted.text)
+  render.text <- append(render.text, ...)
   template <- paste(readLines(template.file ),collapse = '\n')
   cat(whisker::whisker.render(template, render.text), file = file.out)
   xml <- xml2::read_xml(file.out)
