d0b1b5e3a37ff81a99f702fae07af649837acbab
Jordan S Read <jread@usgs.gov>
2016-10-27 13:41:54 -0500

    update to crosswalk mapping

--- scripts/depth_link_swimms.R
+++ scripts/depth_link_swimms.R
@@ -1,17 +1,16 @@
 
-depth_link_swimms = function(fname){
+depth_link_swimms = function(fname, crosswalk){
 	
-	swimms = read.table(fname, header=TRUE, sep='\t', as.is=TRUE, comment.char = "")
+  load(crosswalk)
+
+	swimms = read.table(fname, header=TRUE, sep='\t', as.is=TRUE, comment.char = "") %>% 
+	  dplyr::mutate(zmax = Official.Max.Depth * 0.3048, type='maxdepth', source='swimms') %>% 
+	  dplyr::rename(WBIC = Waterbody.ID.Code..WBIC.) %>% 
+	  dplyr::left_join(nhd2wbic, by="WBIC") %>% 
+	  dplyr::rename(id = site_id) %>% 
+	  dplyr::filter(Official.Max.Depth...Units == 'FEET', !is.na(zmax), !is.na(id)) %>% 
+	  dplyr::select(id, source, type, zmax)
 	
-	swimms$id = link_to_nhd(swimms$Latitude, swimms$Longitude)
-	
-	swimms$zmax = swimms$Official.Max.Depth
-	swimms$zmax[swimms$Official.Max.Depth...Units == 'FEET'] = swimms$zmax[swimms$Official.Max.Depth...Units == 'FEET'] * 0.3048
-	
-	swimms$`source` = 'swimms'
-	swimms$type     = 'maxdepth'
-	
-	to_write        = swimms[,c('id', 'source', 'type', 'zmax')]
-	write.table(to_write, 'data/depth_data_linked/depth_swimms_wisconsin.csv', sep=',', row.names=FALSE)
+	write.table(swimms, 'data/depth_data_linked/depth_swimms_wisconsin.csv', sep=',', row.names=FALSE)
 	
 }
